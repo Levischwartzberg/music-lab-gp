6a07af713a7b8a05adb5c8e3228f296a81e30a96
Levischwartzberg <52432747+Levischwartzberg@users.noreply.github.com>
2021-04-22 23:41:30 -0500

    Update deets.js
    looks like the conflicts weren't properly resolved (there was duplicate material and the ======= sign).

--- connection/deets.js
+++ connection/deets.js
@@ -1,18 +1,3 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
-=======
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
